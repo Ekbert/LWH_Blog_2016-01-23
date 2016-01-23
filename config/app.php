7cd3d475ee807e7636b9dd08f6c0c95055eb6cd2
Ekbert <49943105@gm.nfu.edu.tw>
2016-01-23 14:56:15 +0800

    practice2 done (tracy&terminal)

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Recca0120\LaravelTracy\ServiceProvider::class,
+        Recca0120\Terminal\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
